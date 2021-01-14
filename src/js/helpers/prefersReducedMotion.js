ece9163206e68debb558f73cfdeae945d2336505
= <=>
2021-01-14 13:51:19 +0100

    remove console.log

--- src/js/helpers/prefersReducedMotion.js
+++ src/js/helpers/prefersReducedMotion.js
@@ -1,6 +1,5 @@
 function prefersReducedMotion() {
   const mediaQueryList = window.matchMedia("(prefers-reduced-motion: reduce)");
-  console.log(mediaQueryList.matches);
   return mediaQueryList.matches;
 }
 
